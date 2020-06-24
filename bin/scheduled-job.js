fe218ac4d46464f1bc33b0b11327e39de8a82386
Duncan Harvey <duncanpharvey@gmail.com>
2020-06-24 16:36:27 -0400

    fixes async issue

--- bin/scheduled-job.js
+++ bin/scheduled-job.js
@@ -2,5 +2,9 @@
 const app = require('../app.js');
 const app2 = require('../app2.js');
 
-await app.run();
-await app2.main();
+async function main() {
+    await app.run();
+    await app2.main();
+}
+
+main();
